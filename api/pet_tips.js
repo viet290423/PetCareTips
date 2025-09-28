a41c12c789f897949ec728bcb51ece451e0b9f3f
NguyenDucViet-Phenet <ducnv@phenikaa-x.com>
2025-09-28 21:33:45 +0700

    change generativeModel

--- api/pet_tips.js
+++ api/pet_tips.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
